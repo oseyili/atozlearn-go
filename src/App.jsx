1f3761bdf26316b4619a8aac4f2fda29e0f28cd1
Your Name <you@example.com>
2026-01-24 21:42:08 +0000

    Remove old portal (latest 200) and force MasterPortal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 ﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MasterPortal from "./pages/MasterPortal";
-import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/portal" element={<MasterPortal />} />
         <Route path="/" element={<Navigate to="/portal" replace />} />
         <Route path="*" element={<Navigate to="/portal" replace />} />
